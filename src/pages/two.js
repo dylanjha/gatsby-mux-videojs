5b15aabb4dae5ec1f6959ea9c0b70762ab16d634
Blake Petetan <blake@blake.me>
2020-12-14 15:58:06 -0800

    reproducing log warn error

--- src/pages/two.js
+++ src/pages/two.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import { navigate } from 'gatsby';
+import Player from '../components/player2';
 
 const Two = () => {
   return (
     <>
       <h2>Page Two</h2>
+      <Player />
       <button onClick={() => navigate('/')}>Go Back</button>
     </>
   );
